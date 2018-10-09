3a3e042d112c98b6f853e0a9bf1a8418101977d9
Abby Wessels <abbywessels@quickenloans.com>
2018-10-09 15:10:30 -0400

    first commit

---
+++ GC_Lab04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lab04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = true;
+
+            while (cont == true)
+            { 
+                Console.WriteLine("Give me a number");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Number" + "\t" + "Squared" + "\t" + "Cubed");
+                Console.WriteLine("===== \t ===== \t =====");
+                Console.WriteLine(Math.Pow(num, 1) + "\t" + Math.Pow(num, 2) + "\t" + Math.Pow(num, 3));
+
+                Console.WriteLine("Continue? (y/n)");
+                string ans = Console.ReadLine();
+                if (ans == "n") { cont = false; }
+            }
+        }
+    }
+}
